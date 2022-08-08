db6b8df5e60c8280bb3ca5883ae92b40d7fff088
Sadiq <sadik.ahmad@itcglobal.ng>
2022-08-08 10:39:11 +0100

    View Engines EJS

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const express = require('express')
+
+// create an express app
 const app = express()
+
+//resgister view engine
+app.set('view engine', 'ejs')
+// app.set('views', './views')
 
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) =>{
-    // res.send('<h2> Home page </h2>');
-    res.sendFile('./views/index.html', {root: __dirname});
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quos.'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quos.'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quos.'},
+    ];
+    res.render('index', {title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) =>{
-    // res.send('<h2> About page </h2>');
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About Page'});
 });
 
-//redirects
-app.get('/about-us', (req, res) =>{
-    res.redirect('/about');
-});   //redirects to /about
+app.get('/blogs/create', (req, res) =>{
+     res.render('create',   {title: 'Create Blog'});
+}); 
 
 //404
 app.get ((req, res) =>{
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404',   {title: '404'});
 });
-
-// const port = 3000 
-
-// app.get('/', (req, res) => res.send('Hello World!'))
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
